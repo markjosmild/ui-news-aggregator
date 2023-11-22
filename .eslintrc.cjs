34fc221d51ba1a3e1e31cc9c700a0f1e39f5cbd6
Mark Josmil Degamo <markjosmild@gmail.com>
2023-11-23 04:57:57 +0800

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:vue/vue3-essential'
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      },
+      rules: {}
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: { 'vue/multi-word-component-names': 'off' }
+}
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const navs = ref([
+  { name: 'home', path: { name: 'home' } },
+  { name: 'about', path: { name: 'about' } }
+])
+</script>
+
+<template>
+  <div class="bg-black p-5 uppercase font-semibold text-white flex items-center justify-center gap-5">
+    <router-link v-for="(nav, idx) in navs" :key="idx" :to="nav.path">
+      <span :class="`hover:text-yellow-500 ${route.name === nav.path.name && 'text-yellow-400'}`">
+        {{ nav.name }}
+      </span>
+    </router-link>
+  </div>
+  <div class="text-center">
+    <router-view />
+  </div>
+</template>
+
+<style scoped></style>
